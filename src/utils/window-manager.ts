cba1d5747aa32fd8f84fa0c9f3cd80d786dee8ca
Callum <Calxferg@gmail.com>
2021-08-18 11:11:02 -0400

    misc

--- src/utils/window-manager.ts
+++ src/utils/window-manager.ts
@@ -82,8 +82,6 @@ export default class WindowManager {
 
   allTabViews: Record<number, TabView> = {};
 
-  // tabId = 0; // auto increment to give unique id to each tab
-
   activeTabId = -1;
 
   findText = '';
@@ -386,7 +384,6 @@ export default class WindowManager {
   }
 
   createNewTab(): number {
-    // this.tabId += 1;
     const newTabView = new TabView(
       this.mainWindow,
       this.titleBarView,
