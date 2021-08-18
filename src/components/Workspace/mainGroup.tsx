b3410d2aa6566099aeeb6078189d7d1b077fd707
Callum <Calxferg@gmail.com>
2021-08-18 11:36:02 -0400

    refactor workspace

---
+++ src/components/Workspace/mainGroup.tsx
@@ -0,0 +1,252 @@
+import styled from 'styled-components';
+import { observer } from 'mobx-react-lite';
+import { Instance } from 'mobx-state-tree';
+import React, { useEffect, useRef } from 'react';
+import { runInAction } from 'mobx';
+import { DraggableCore, DraggableData } from 'react-draggable';
+import {
+  groupPadding,
+  groupTitleHeight,
+  ItemGroup,
+  widthPixelsToInt,
+} from '../../store/workspace-store';
+import { useStore } from '../../store/tab-page-store';
+import { easeOut, overTrash } from './utils';
+import { lerp } from '../../utils/utils';
+
+const Group = styled.div`
+  background-color: rgb(255, 170, 166);
+  border-radius: 20px;
+  color: rgb(250, 250, 250);
+  position: absolute;
+  border: 2px solid black;
+`;
+const GroupResize = styled.div`
+  width: 20px;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  right: -10px;
+
+  :hover {
+    cursor: col-resize;
+  }
+`;
+const GroupHeader = styled.div`
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+  position: relative;
+  outline: none;
+`;
+const HeaderText = styled.div`
+  position: absolute;
+  top: -2px;
+  left: 0;
+  width: 100%;
+  padding-left: 12px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  font-size: 2rem;
+  font-weight: bold;
+  color: rgb(250, 250, 250);
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+`;
+const HeaderInput = styled.input`
+  position: absolute;
+  top: -4px;
+  left: 0;
+  width: 100%;
+  padding-left: 12px;
+  font-size: 2rem;
+  font-weight: bold;
+  outline: none;
+  border: none;
+  background: none;
+  color: rgb(250, 250, 250);
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+`;
+const MainGroup = observer(
+  ({ group }: { group: Instance<typeof ItemGroup> }) => {
+    const { tabPageStore, workspaceStore } = useStore();
+
+    const targetGroupSize = group.size();
+    const lerpValue = easeOut(group.animationLerp);
+
+    const groupTitleBoxRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+      if (tabPageStore.editingGroupId === group.id) {
+        setTimeout(() => {
+          groupTitleBoxRef.current?.select();
+        }, 10);
+      }
+    }, [tabPageStore.editingGroupId, group.id]);
+
+    useEffect(() => {
+      if (group.shouldEditTitle) {
+        group.setShouldEditTitle(false);
+        runInAction(() => {
+          tabPageStore.activeGroupBoxRef = groupTitleBoxRef;
+          tabPageStore.editingGroupId = group.id;
+        });
+        if (groupTitleBoxRef.current !== null) {
+          groupTitleBoxRef.current.value = group.title;
+        }
+      }
+    }, [group, group.shouldEditTitle, tabPageStore]);
+
+    return (
+      <DraggableCore
+        onStart={(_, data) => {
+          workspaceStore.moveToFront(group);
+          group.setDragMouseStart(data.x, data.y);
+
+          if (data.x > group.x + group.size()[0] - 10) {
+            group.setTempResizeWidth(group.width);
+            group.setResizing(true);
+          } else if (data.y > group.y + groupTitleHeight + groupPadding + 1) {
+            group.setBeingDragged(true);
+            workspaceStore.setAnyDragging(true);
+          }
+        }}
+        onDrag={(_, data: DraggableData) => {
+          if (group.resizing) {
+            group.setTempResizeWidth(widthPixelsToInt(data.x - group.x));
+            workspaceStore.setGroupWidth(
+              Math.floor(group.tempResizeWidth),
+              group
+            );
+          } else {
+            if (
+              !group.beingDragged &&
+              tabPageStore.editingGroupId !== group.id
+            ) {
+              const xDif = data.x - group.dragMouseStartX;
+              const yDif = data.y - group.dragMouseStartY;
+              const distSquared = xDif * xDif + yDif * yDif;
+              if (distSquared > 5 * 5) {
+                group.setBeingDragged(true);
+                workspaceStore.setAnyDragging(true);
+              }
+            }
+
+            if (group.beingDragged) {
+              group.setOverTrash(overTrash([data.x, data.y], workspaceStore));
+              workspaceStore.setAnyOverTrash(group.overTrash);
+              group.move(data.deltaX, data.deltaY);
+            }
+          }
+        }}
+        onStop={(_, data) => {
+          if (
+            !group.beingDragged &&
+            !group.resizing &&
+            tabPageStore.editingGroupId !== group.id
+          ) {
+            runInAction(() => {
+              tabPageStore.activeGroupBoxRef = groupTitleBoxRef;
+              tabPageStore.editingGroupId = group.id;
+            });
+            if (groupTitleBoxRef.current !== null) {
+              groupTitleBoxRef.current.value = group.title;
+            }
+          }
+
+          if (group.resizing) {
+            const roundFunc = group.height() === 1 ? Math.round : Math.floor;
+            group.setTempResizeWidth(widthPixelsToInt(data.x - group.x));
+            workspaceStore.setGroupWidth(
+              roundFunc(group.tempResizeWidth),
+              group,
+              true
+            );
+            group.setResizing(false);
+          }
+
+          if (group.overTrash) {
+            workspaceStore.deleteGroup(group);
+            workspaceStore.setAnyDragging(false);
+            workspaceStore.setAnyOverTrash(false);
+            return;
+          }
+
+          group.setBeingDragged(false);
+          group.setOverTrash(false);
+          workspaceStore.setAnyDragging(false);
+          workspaceStore.setAnyOverTrash(false);
+        }}
+      >
+        <Group
+          style={{
+            width: lerp(
+              group.animationStartWidth,
+              targetGroupSize[0],
+              lerpValue
+            ),
+            height: lerp(
+              group.animationStartHeight,
+              targetGroupSize[1],
+              lerpValue
+            ),
+            left: group.x,
+            top: group.y,
+            zIndex: group.zIndex,
+            display: group.id === 'hidden' ? 'none' : 'block',
+            cursor: group.beingDragged ? 'grabbing' : 'auto',
+          }}
+          onMouseOver={() => {
+            group.setHovering(true);
+          }}
+          onMouseLeave={() => {
+            group.setHovering(false);
+          }}
+        >
+          <GroupHeader
+            style={{
+              height: groupTitleHeight + groupPadding,
+              cursor: group.beingDragged ? 'grabbing' : 'pointer',
+            }}
+          >
+            <HeaderText
+              style={{
+                display:
+                  tabPageStore.editingGroupId === group.id ? 'none' : 'block',
+              }}
+            >
+              {group.title}
+            </HeaderText>
+            <HeaderInput
+              ref={groupTitleBoxRef}
+              type="text"
+              spellCheck="false"
+              style={{
+                display:
+                  tabPageStore.editingGroupId === group.id ? 'block' : 'none',
+                height: groupTitleHeight + groupPadding,
+              }}
+              onMouseDown={(e) => {
+                e.stopPropagation();
+              }}
+              onBlur={(e) => {
+                runInAction(() => {
+                  tabPageStore.activeGroupBoxRef = null;
+                  tabPageStore.editingGroupId = '';
+                });
+                if (e.currentTarget.value !== '') {
+                  group.setTitle(e.currentTarget.value);
+                }
+              }}
+            />
+          </GroupHeader>
+          <GroupResize />
+        </Group>
+      </DraggableCore>
+    );
+  }
+);
+
+export default MainGroup;
---
+++ src/components/Workspace/mainItem.tsx
@@ -0,0 +1,229 @@
+import styled, { css } from 'styled-components';
+import { observer } from 'mobx-react-lite';
+import { Instance } from 'mobx-state-tree';
+import React from 'react';
+import { DraggableCore, DraggableData } from 'react-draggable';
+import { runInAction } from 'mobx';
+import { ipcRenderer } from 'electron';
+import {
+  groupPadding,
+  groupTitleHeight,
+  Item as MobxItem,
+  ItemGroup,
+  itemHeight,
+  itemWidth,
+} from '../../store/workspace-store';
+import { useStore } from '../../store/tab-page-store';
+import { easeOut, getGroupBelowItem, overTrash } from './utils';
+import { lerp } from '../../utils/utils';
+
+const ItemPlaceholderAndContainer = styled.div``;
+const ItemPlaceholder = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+`;
+const ItemContainer = styled.div`
+  background-color: white;
+  border-radius: 20px;
+  color: rgb(50, 50, 50);
+  position: absolute;
+  transition: transform 0.05s ease-out, filter 0.25s;
+  overflow: hidden;
+
+  ${({ showTitle }: { showTitle: boolean }) =>
+    css`
+      div {
+        opacity: ${showTitle ? '100' : '0'};
+      }
+    `};
+`;
+const ItemImg = styled.img`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  background-color: white;
+
+  // :(
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  user-select: none;
+  -webkit-user-drag: none;
+`;
+const ItemTitle = styled.div`
+  background-color: rgba(0, 0, 0, 0.6);
+  color: white;
+  position: absolute;
+  font-size: 0.9rem;
+  width: calc(100% - 10px);
+  height: calc(100% - 10px);
+  padding: 5px;
+  overflow: hidden;
+  top: 0;
+  left: 0;
+  opacity: 0;
+  transition: opacity 0.25s;
+`;
+const MainItem = observer(
+  ({
+    group,
+    item,
+  }: {
+    group: Instance<typeof ItemGroup>;
+    item: Instance<typeof MobxItem>;
+  }) => {
+    const { tabPageStore, workspaceStore } = useStore();
+    const targetPos = item.placeholderPos(group);
+    const lerpValue = easeOut(item.animationLerp);
+
+    return (
+      <ItemPlaceholderAndContainer>
+        <ItemPlaceholder
+          style={{
+            width: itemWidth,
+            height: itemHeight,
+            left: targetPos[0],
+            top: targetPos[1],
+            zIndex: group.zIndex,
+          }}
+        />
+        <DraggableCore
+          onMouseDown={(e) => {
+            e.stopPropagation();
+          }}
+          onStart={(_, data: DraggableData) => {
+            item.setDragMouseStart(data.x, data.y);
+            workspaceStore.moveToFront(group);
+            group.setHovering(false);
+          }}
+          onDrag={(_, data: DraggableData) => {
+            if (!item.beingDragged) {
+              const xDif = data.x - item.dragMouseStartX;
+              const yDif = data.y - item.dragMouseStartY;
+              const distSquared = xDif * xDif + yDif * yDif;
+              if (distSquared > 5 * 5) {
+                item.setBeingDragged(true);
+                workspaceStore.setAnyDragging(true);
+                item.setDragStartGroup(group.id);
+                item.setContainerDragPos(targetPos);
+              }
+            }
+
+            if (item.beingDragged) {
+              item.setContainerDragPos([
+                item.containerDragPosX + data.deltaX,
+                item.containerDragPosY + data.deltaY,
+              ]);
+              item.setOverTrash(overTrash([data.x, data.y], workspaceStore));
+              workspaceStore.setAnyOverTrash(item.overTrash);
+              if (item.overTrash) {
+                if (group.id !== 'hidden') {
+                  workspaceStore.changeGroup(
+                    item,
+                    group,
+                    workspaceStore.hiddenGroup
+                  );
+                }
+              } else {
+                getGroupBelowItem(
+                  item,
+                  group,
+                  [item.containerDragPosX, item.containerDragPosY],
+                  workspaceStore
+                );
+              }
+            }
+          }}
+          onStop={() => {
+            let newGroup = group;
+            if (!item.beingDragged) {
+              runInAction(() => {
+                tabPageStore.workspaceActive = false;
+              });
+              ipcRenderer.send('open-workspace-url', item.url);
+            } else {
+              if (!item.overTrash) {
+                const groupBelow = getGroupBelowItem(
+                  item,
+                  group,
+                  [item.containerDragPosX, item.containerDragPosY],
+                  workspaceStore
+                );
+                if (groupBelow === null) {
+                  const createdGroup = workspaceStore.createGroup('New Group');
+                  newGroup = createdGroup;
+                  createdGroup.move(
+                    item.containerDragPosX - groupPadding,
+                    item.containerDragPosY - (groupPadding + groupTitleHeight)
+                  );
+                  workspaceStore.changeGroup(item, group, createdGroup);
+                  createdGroup.setShouldEditTitle(true);
+                }
+              }
+
+              item.setContainerDragPos(targetPos);
+
+              if (item.dragStartGroup !== '') {
+                const startGroup = workspaceStore.groups.get(
+                  item.dragStartGroup
+                );
+                if (
+                  typeof startGroup !== 'undefined' &&
+                  startGroup.itemArrangement.length === 0
+                ) {
+                  workspaceStore.deleteGroup(startGroup);
+                }
+              }
+              item.setDragStartGroup('');
+              item.setBeingDragged(false);
+
+              if (item.overTrash) {
+                workspaceStore.deleteItem(item, group);
+              }
+            }
+            workspaceStore.setAnyDragging(false);
+            workspaceStore.setAnyOverTrash(false);
+            newGroup.setHovering(true);
+          }}
+        >
+          <ItemContainer
+            showTitle={
+              group.hovering &&
+              !workspaceStore.anyDragging &&
+              !group.resizing &&
+              tabPageStore.editingGroupId !== group.id
+            }
+            style={{
+              width: itemWidth,
+              height: itemHeight,
+              left: item.beingDragged
+                ? item.containerDragPosX
+                : lerp(item.animationStartX, targetPos[0], lerpValue),
+              top: item.beingDragged
+                ? item.containerDragPosY
+                : lerp(item.animationStartY, targetPos[1], lerpValue),
+              zIndex: item.beingDragged ? 10000000 : group.zIndex,
+              transform: item.beingDragged ? 'rotate(5deg)' : 'none',
+              cursor: item.beingDragged ? 'grabbing' : 'default',
+              // opacity: item.overTrash ? 0.5 : 1,
+              boxShadow: item.beingDragged
+                ? '0 0 5px 0 rgba(100, 100, 100, 0.5)'
+                : 'none',
+            }}
+            onMouseOver={() => {
+              group.setHovering(true);
+            }}
+            onMouseLeave={() => {
+              group.setHovering(false);
+            }}
+          >
+            <ItemImg src={item.image} alt="tab_image" />
+            <ItemTitle>{item.title}</ItemTitle>
+          </ItemContainer>
+        </DraggableCore>
+      </ItemPlaceholderAndContainer>
+    );
+  }
+);
+
+export default MainItem;
