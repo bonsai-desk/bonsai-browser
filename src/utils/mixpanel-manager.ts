66ce2e96514591bdc213d98a8d2840d4b8d919a8
Cameron Ferguson <cam@hyferg.com>
2021-09-18 17:08:31 -0400

    track version number

--- src/utils/mixpanel-manager.ts
+++ src/utils/mixpanel-manager.ts
@@ -1,5 +1,6 @@
 import { app } from 'electron';
 import { init, Mixpanel } from 'mixpanel';
+import packageInfo from '../package.json';
 
 const projectDevToken = '88c46eeab06417e9bf2036451269a022';
 const projectToken = 'fa4d29136a737991cef9ae0a39f0b904';
@@ -15,6 +16,9 @@ class MixpanelManager {
     const token = app.isPackaged ? projectToken : projectDevToken;
     this.mixpanel = init(token);
     this.track('boot app');
+    this.mixpanel.people.set(this.userId, {
+      version: packageInfo.version,
+    });
   }
 
   track(eventName: string, properties = {}) {
